cd70098d0a04da57c926a5a632deb645f5effd82
Distro Lucas <lucasassis@outlook.com>
2017-10-19 15:57:53 -0200

    - Added a secondary function for buying tokens, without the withdrawal pattern ,so when a crowdsale ends it can potentially keep selling the tokens and send the tokens directly to the user that bought them.

--- contracts/crowdsale/LimitedTokenCrowdsale.sol
+++ contracts/crowdsale/LimitedTokenCrowdsale.sol
@@ -59,18 +59,17 @@ contract LimitedTokenCrowdSale {
       require(_wallet != 0x0);
 
       initialTokenRate = _rate;
+      saleStartDate = _saleStartDate;
       token = StandardToken(_tokenAddress);
       wallet = _wallet;
       minTokenTransaction = _minTokenTransaction;
-      saleStartDate = _saleStartDate;
   }
 
   // Receive ETH
   function () payable {
     // block the transaction if the sale hasn't started yet
     require(now > saleStartDate);
-
-    buyToken();
+    buyTokenWithdraw();
   }
 
   /**
@@ -82,8 +81,8 @@ contract LimitedTokenCrowdSale {
     return initialTokenRate;
   }
 
-  // Saves how much the user bought in tokens
-  function buyToken() payable {
+  // Saves how much the user bought in tokens for later withdrawal
+  function buyTokenWithdraw() payable {
     uint256 weiAmount = msg.value;
     address sender = msg.sender;
 
@@ -93,6 +92,7 @@ contract LimitedTokenCrowdSale {
     // give a chance to an inheriting contract to have
     // its own options for token pricing
     uint256 price = getCurrentTokenPrice();
+
     // calculate token amount to be given to user
     // (Solidity always truncates on division)
     uint256 tokens = weiAmount / price;
@@ -116,6 +116,43 @@ contract LimitedTokenCrowdSale {
 
     // send an event for a Token Purchase
     TokenPurchase(sender, sender, weiAmount, tokens);
+  }
+
+  // Buys tokens and sends them directly to the end user
+  function buyTokenDirect() payable {
+    uint256 weiAmount = msg.value;
+    address sender = msg.sender;
+
+    require(sender != 0x0);
+    require(weiAmount != 0);
+
+    // give a chance to an inheriting contract to have
+    // its own options for token pricing
+    uint256 price = getCurrentTokenPrice();
+
+    // calculate token amount to be given to user
+    // (Solidity always truncates on division)
+    uint256 tokens = weiAmount / price;
+
+    // must have more tokens than min transaction..
+    require(tokens > minTokenTransaction);
+    // ..and the contract must have tokens to sell
+    require(token.balanceOf(this) > tokens);
+
+    // add to the balance of the user, to be paid later
+    if (token.transfer(sender, tokens)) {
+      // update how much Wei we have raised
+      weiRaised = weiAmount.add(weiRaised);
+      // update the total amount of tokens we have sold
+      // (this value never goes down)
+      tokensSold = tokensSold.add(tokens);
+      // updates the total amount of tokens we have to still withdraw,
+      // this will be subtracted each time an user withdraws
+      tokensToWithdraw = tokensToWithdraw.add(tokens);
+
+      // send an event for a Token Purchase
+      TokenPurchase(sender, sender, weiAmount, tokens);
+    }
   }
 
   // Withdraws the tokens that the sender owns
